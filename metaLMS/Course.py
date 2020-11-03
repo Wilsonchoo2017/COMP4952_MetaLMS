a5e4c03ec9f7505c10fabbe102e5b05a96254154
EC2 Default User <ec2-user@ip-172-31-35-28.ap-southeast-2.compute.internal>
2020-11-03 09:44:20 +0000

    update requirements

--- metaLMS/Course.py
+++ metaLMS/Course.py
@@ -1,11 +1,8 @@
 from .utility.database import insert_course_into_database, max_course_id, get_course_and_associated_details_from_db, \
     get_is_imported
-from .utility.scorm_utils import *
 from base64 import b64decode
 import time, threading
 from .LearningObject import *
-
-job_queue = [{"job_id": "QKZXU3B8GG1", "course_id": "1"}]
 
 
 def handle_post_course(ontology_file, course_json):
@@ -18,38 +15,6 @@ def handle_post_course(ontology_file, course_json):
     else:
         # import into scorm
         handle_import_course(ontology_file, course_json)
-
-def handle_import_course(ontology_file, course_json):
-    print(course_json)
-
-    # Save File
-    file = course_json['file']
-    file = file.split(",")[1]
-    bytes = b64decode(file, validate=True)
-
-    curr_id = max_course_id() + 1
-    save_filepath = '/Users/wilson/PycharmProjects/COMP4952/LearningObject/' + "Course" + str(curr_id)
-    print(save_filepath)
-    f = open(save_filepath, 'wb')
-    f.write(bytes)
-    f.close()
-
-    # Save into Database
-    job_id = upload_course(save_filepath)
-    job_queue.append({"job_id": job_id, "course_id": curr_id})
-    return job_id
-
-def check_import_job_status(job_id):
-    response_json = check_job_status(job_id)
-    status = response_json.status
-    if status == "COMPLETE":
-        return 0
-
-    if status == "RUNNING":
-        return 1
-
-    # Error
-    return 2
 
 def handle_upload_complete_course(course_id):
     response = get_scorm_course_detail(course_id)
@@ -75,22 +40,6 @@ def get_course_detail(course_id):
     else:
         return get_scorm_course_detail(course_id)
 
-
-def ping_job_status():
-    print(time.ctime())
-    if len(job_queue) is not 0:
-        print(job_queue)
-        job = job_queue[0]
-        job_id = job.get("job_id")
-        course_id = job.get("course_id")
-        if check_job_status(job_id) == 0:
-            # remove from queue
-            job_queue.pop(0)
-            # Do stuff with the course_id
-            handle_upload_complete_course(course_id)
-
-    # do this again in 10 seconds
-    threading.Timer(10, ping_job_status).start()
 
 def get_ssm_of_two_courses(course_a, course_b):
     response_a = get_course_detail(course_a)['LearningObject']
